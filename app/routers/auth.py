555fbcd19c6d9d096ac05f289a0b8fb2b36b6049
XimenaLH21 <ximena@dicalzeus.com>
2025-11-20 12:37:03 -0700

    feat: add endpoints to register users and login

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
 from app.db.database_setup import engine
-from app.models.models import User
+from app.models.models import User, ResponseGlobal, LoginBody
+from app.services.encryption_service import password_hash, checking_password, create_jwt_token
+from datetime import datetime, timedelta, timezone
 
 router = APIRouter(
     prefix="/api/v1",
@@ -12,11 +14,30 @@ def get_session():
     with Session(engine) as session:
         yield session
 
-@router.get("/login/sign-in")
-async def sign_in(session: Session = Depends(get_session)):
-    
-    return session.exec(select(User)).all()
+@router.post("/login/sign-in")
+async def sign_in(body: LoginBody, session: Session = Depends(get_session)):
+    query = select(User).where(User.email == body.email)
+    user_found = session.exec(query).one()
 
-@router.post("login/register")
-async def register(session: Session = Depends(get_session)):
-    return "Register"
+    is_correct_pw = checking_password(body.password, user_found.password)
+    user_dict = user_found.model_dump(exclude={"password"})
+    user_dict["exp"] = datetime.now(timezone.utc) + timedelta(hours=1)
+    print(user_dict)
+    if is_correct_pw:
+        token = create_jwt_token(user_dict)
+        print(" TOKEN ", token)
+        user_dict["token"] = token
+
+    return {"success":True, "message":"Se logeó exitosamente", "data": user_dict}
+
+@router.post("/login/register")
+async def register(body: User, session: Session = Depends(get_session)) -> ResponseGlobal :
+    pw = body.password
+    hash_pw = password_hash(pw)
+    body.password = hash_pw
+    
+    session.add(body)
+    session.commit()
+    session.refresh(body)
+
+    return ResponseGlobal(success=True, message="Se registró el usuario exitosamente", data=body)
