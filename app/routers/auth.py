62e66d2e4acdbd632f0ccc90c85a8115db84bf8d
XimenaLH21 <ximena@dicalzeus.com>
2025-11-21 16:03:47 -0700

    refactor: change the response of login endpoint

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
 from app.db.database_setup import engine
-from app.models.models import User, ResponseGlobal, LoginBody
+from app.models.models import User, ResponseGlobal, LoginBody, AuthorizationResponse
 from app.services.encryption_service import password_hash, checking_password, create_jwt_token
 from datetime import datetime, timedelta, timezone
 
 router = APIRouter(
-    prefix="/api/v1",
+    prefix="/login",
     tags=["Auth"]
 )
 
@@ -14,23 +14,30 @@ def get_session():
     with Session(engine) as session:
         yield session
 
-@router.post("/login/sign-in")
+@router.post("/sign-in", status_code=200)
 async def sign_in(body: LoginBody, session: Session = Depends(get_session)):
     query = select(User).where(User.email == body.email)
     user_found = session.exec(query).one()
 
     is_correct_pw = checking_password(body.password, user_found.password)
     user_dict = user_found.model_dump(exclude={"password"})
-    user_dict["exp"] = datetime.now(timezone.utc) + timedelta(hours=1)
-    print(user_dict)
+    user_dict["exp"] = int((datetime.now(timezone.utc) + timedelta(hours=1)).timestamp())
+
     if is_correct_pw:
         token = create_jwt_token(user_dict)
-        print(" TOKEN ", token)
         user_dict["token"] = token
 
-    return {"success":True, "message":"Se logeó exitosamente", "data": user_dict}
+    auth_response = AuthorizationResponse(
+        user_id=user_dict["id"],
+        name=user_dict["name"],
+        email=user_dict["email"],
+        exp=user_dict["exp"],
+        token=user_dict["token"]
+    )
 
-@router.post("/login/register")
+    return ResponseGlobal(success=True, message="Se logeó exitosamente", data=auth_response)
+
+@router.post("/register", status_code=201)
 async def register(body: User, session: Session = Depends(get_session)) -> ResponseGlobal :
     pw = body.password
     hash_pw = password_hash(pw)
